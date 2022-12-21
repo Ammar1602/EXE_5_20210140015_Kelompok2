9fa3130ba9870f2a15572f949367b3f66c7bdd8c
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-12-21 16:53:08 +0700

    add program

--- EXE_5_20210140015_Kelompok2/Program.cs
+++ EXE_5_20210140015_Kelompok2/Program.cs
@@ -5,134 +5,126 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EXE_5_20210140015_Kelompok2
+namespace Execrise5_20170140031_Kelompok2
 {
-    class Queues
-    {
-        int Ammar, Syuhada, max = 10;
-        int[] queue_array = new int[10];
-        public Queues()
-        {
-            Ammar = -1;
-            Syuhada = -1;
-        }
 
-        public void insert(int element)
-        {
-            if (Ammar == -1)
-            {
-                Ammar = 0;
-                Syuhada = 0;
-            }
-            else
-            {
-                if (Syuhada == max - 1)
-                    Syuhada = 0;
-                else
-                    Syuhada = Syuhada + 1;
-            }
-            queue_array[Syuhada] = element;
-        }
-        public void delete()
-        {
-            if (Ammar == -1)
-            {
-                Console.WriteLine("Queue underflow\n");
-                return;
-            }
-            Console.WriteLine("\nThe element deleted from the queueis:" + queue_array[Ammar] + "\n");
-            if (Ammar == Syuhada)
-            {
-                Ammar = -1;
-                Syuhada = -1;
-            }
-            else
-            {
-                if (Ammar == max - 1)
-                    Ammar = 0;
-                else
-                    Ammar = Ammar + 1;
-            }
-        }
-        public void display()
-        {
-            int Ammar_position = Ammar;
-            int Syuhada_position = Syuhada;
-            if (Ammar == -1)
-            {
-                Console.WriteLine("Queue is empty\n");
-                return;
-            }
-            Console.WriteLine("\nElement is the queue are...\n");
-            if (Ammar_position <= Syuhada_position)
-            {
-                while (Ammar_position <= Syuhada_position)
-                {
-                    Console.Write(queue_array[Ammar_position] + " ");
-                    Ammar_position++;
-                }
-                Console.WriteLine();
-            }
-            else
-            {
-                while (Ammar_position <= max - 1)
-                {
-                    Console.Write(queue_array[Ammar_position] + " ");
-                    Ammar_position++;
-                }
-                Ammar_position = 0;
-                while (Ammar_position <= Syuhada_position)
-                {
-                    Console.Write(queue_array[Ammar_position] + " ");
-                    Ammar_position++;
-                }
-                Console.WriteLine();
-            }
-        }
+    class Program
+    {
         static void Main(string[] args)
         {
-            Queues q = new Queues();
-            char ch;
+            Stacks st = new Stacks();
+            st.Mulai();
+        }
+    }
+
+    class Antrian
+    {
+        public string nama;
+        public Antrian next;
+        public Antrian(string n, Antrian d)
+        {
+            nama = n;
+            next = d;
+        }
+    }
+
+    class Stacks
+    {
+        Antrian depan;
+        Antrian belakang;
+        public Stacks()
+        {
+            depan = null;
+            belakang = null;
+        }
+        bool Antrian_Kosong()
+        {
+            if (depan == null)
+                return (true);
+            else
+                return (false);
+        }
+
+        public void masukkan(string nama)
+        {
+            Antrian baru;
+            baru = new Antrian(nama, null);
+            if (depan == null)
+            {
+                depan = baru;
+                belakang = baru;
+            }
+            else
+            {
+                belakang.next = baru;
+                belakang = belakang.next;
+            }
+
+            Console.WriteLine("\nmasuk" + nama + " dalam antrina.");
+        }
+
+        public void keluarkan()
+        {
+            Console.WriteLine("\nAntrian Selesai: " + depan.nama);
+            depan = depan.next;
+        }
+
+        public void display()
+        {
+            Antrian sementara;
+            if (Antrian_Kosong())
+                Console.WriteLine("\ntidak ada antrian");
+            else
+            {
+                Console.WriteLine();
+                for (sementara = depan; sementara != null; sementara = sementara.next)
+                {
+                    Console.WriteLine(sementara.nama);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Mulai()
+        {
+            Stacks s = new Stacks();
             while (true)
             {
-                try
+                Console.WriteLine();
+                Console.WriteLine("1. tambah Element.");
+                Console.WriteLine("2. Hapus Element");
+                Console.WriteLine("3. Tampilkan");
+                Console.WriteLine("4. Keluar");
+                Console.Write("\npilihan: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                Pilihan(ch);
+            }
+
+            void Pilihan(char ch)
+            {
+                switch (ch)
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine("1. Implement Inssert Option");
-                    Console.WriteLine("2. Implement Delete Option");
-                    Console.WriteLine("3. Display Values");
-                    Console.WriteLine("4. Exit");
-                    Console.WriteLine("\nEnter Your Choice (1-4): ");
-                    ch = Convert.ToChar(Console.ReadLine());
-                    Console.WriteLine();
-                    switch (ch)
-                    {
-                        case '1':
-                            {
-                                Console.Write("Enter a number: ");
-                                int string = Convert.ToInt32(System.Console.ReadLine());
-                                Console.WriteLine();
-                                q.insert(string);
-                            }
+                    case '1':
+                        Console.Write("\nTulis sebuah Nama: ");
+                        string num = Console.ReadLine();
+                        s.masukkan(num);
+                        break;
+                    case '2':
+                        if (s.Antrian_Kosong())
+                        {
+                            Console.WriteLine("\nIsi kosong");
                             break;
-                        case '2':
-                            {
-                                q.delete();
-                            }
-                            break;
-                        case '3':
-                            {
-                                q.display();
-                            }
-                            break;
-                        case '4':
-                            return;
-                        default:
-                            {
-                                Console.WriteLine("Invalid Option!");
-                            }
-                            break;
-                    }
+                        }
+                        s.keluarkan();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nSalah Pilih");
+                        break;
                 }
             }
         }
